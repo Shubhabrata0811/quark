e9f4a50553e6e333ba9532ba47292cbee45fa721
Shubhabrata Ghosh <shubhabrata.g1108@gmail.com>
2025-02-20 01:32:04 +0530

    Update the add command, working for both files and folders recursively

--- src/commands/add.js
+++ src/commands/add.js
@@ -1,16 +1,8 @@
 import fs from "fs";
 import path from "path";
 
-export const add = function (filePaths) {
-  if (filePaths.length === 0) {
-    console.log("Nothing specified, nothing added.");
-    return;
-  }
-
-  const currentDirPath = process.cwd();
-  const stagingJSONPath = path.join(currentDirPath, ".quark/staging.json");
+const getStagingData = (stagingJSONPath) => {
   let stagingDataObj = { stagedFiles: [] };
-  const stagedFiles = [];
 
   if (!fs.existsSync(stagingJSONPath)) {
     fs.writeFileSync(stagingJSONPath, JSON.stringify(stagingDataObj));
@@ -18,23 +10,69 @@ export const add = function (filePaths) {
     stagingDataObj = JSON.parse(fs.readFileSync(stagingJSONPath));
   }
 
-  for (let filePath of filePaths) {
-    const absoluteFilePath = path.join(currentDirPath, filePath);
-    if (!fs.existsSync(absoluteFilePath)) {
-      console.log("File not present: %s", filePath);
+  return stagingDataObj;
+};
+
+const stageFile = (filePath, stagingDataObj, stagedFiles) => {
+  if (!stagingDataObj.stagedFiles.includes(filePath)) {
+    stagingDataObj.stagedFiles.push(filePath);
+    stagedFiles.push(filePath);
+  }
+};
+
+const stageFolder = (folderPath, stagingDataObj, stagedFiles) => {
+  const filesPaths = fs.readdirSync(folderPath);
+
+  for (let filePath of filesPaths) {
+    const absoluteFilePath = path.join(folderPath, filePath);
+
+    if (absoluteFilePath === path.join(process.cwd(), ".quark")) {
       continue;
     }
 
-    if (!stagingDataObj.stagedFiles.includes(absoluteFilePath)) {
-      stagingDataObj.stagedFiles.push(absoluteFilePath);
+    if (fs.statSync(absoluteFilePath).isDirectory()) {
+      stageFolder(absoluteFilePath, stagingDataObj, stagedFiles);
+    } else {
+      stageFile(absoluteFilePath, stagingDataObj, stagedFiles);
     }
-    stagedFiles.includes(filePath) || stagedFiles.push(filePath);
+  }
+};
+
+export const add = function (
+  filesPaths,
+  currentDirPath = process.cwd(),
+  stagingJSONPath = path.join(currentDirPath, ".quark/staging.json")
+) {
+  if (filesPaths.length === 0) {
+    console.log("Nothing specified, nothing added.");
+    return;
   }
 
-  fs.writeFileSync(
-    path.join(currentDirPath, ".quark/staging.json"),
-    JSON.stringify(stagingDataObj, null, 2)
-  );
+  let stagingDataObj = getStagingData(stagingJSONPath);
+  const stagedFiles = [];
 
-  console.log("File staged: " + stagedFiles.join(", "));
+  for (let filePath of filesPaths) {
+    const absoluteFilePath = path.join(currentDirPath, filePath);
+
+    if (!fs.existsSync(absoluteFilePath)) {
+      console.log("File not present: %s ---> %s", filePath, currentDirPath);
+      continue;
+    }
+
+    if (absoluteFilePath === path.join(process.cwd(), ".quark")) {
+      continue;
+    }
+
+    if (fs.statSync(absoluteFilePath).isDirectory()) {
+      stageFolder(absoluteFilePath, stagingDataObj, stagedFiles);
+    } else {
+      stageFile(absoluteFilePath, stagingDataObj, stagedFiles);
+    }
+  }
+
+  fs.writeFileSync(stagingJSONPath, JSON.stringify(stagingDataObj, null, 2));
+
+  if (stagedFiles.length > 0) {
+    console.log("File(s) staged:", stagedFiles.join(", ")); //need not to show this
+  }
 };
